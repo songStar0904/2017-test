4e8ccd48b6ed4e689befeb37fc8b9a982b4fc32b
songStar <1043328710@qq.com>
2018-07-22 23:10:37 +0800

    水平垂直居中

--- js实现/commonParentNode.js
+++ js实现/commonParentNode.js
@@ -4,7 +4,8 @@
 // oNode1 和 oNode2 在同一文档中，且不会为相同的节点
 
 //不用去递归
-//也 不用管谁包含谁，只要随便找一个节点，直到某个祖先节点（或自己）包含另一个节点就行了。 oNode.contains(oNode)是等于true的
+//也 不用管谁包含谁，只要随便找一个节点，直到某个祖先节点（或自己）包含另一个节点就行了。 
+//oNode.contains(oNode)是等于true的
 
 function commonParentNode(oNode1, oNode2) {
     for(;oNode1; oNode1 = oNode1.parentNode){
